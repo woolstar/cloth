3419444b5bac2172c7553bbc8078202fef819a66
woolstar <jdwoolverton@gmail.com>
2017-04-16 10:50:31 -0400

    Mommy and me campaign

--- db/user1.sql
+++ db/user1.sql
@@ -1,15 +1,18 @@
 
 insert into identity (id, user, auth, authty ) VALUES ( 100, 'nylulagirl', 'a15f80401240607f', 3 ) ;
 
-insert into campaign (owner_fk, name, tags, art_prefix, isactive) 
+insert into campaign (owner_fk, name, tags, art_prefix, isactive)
 	VALUES ( 100, 'elegant', 'elegant', 'ue', 1 ) ;
 
-insert into campaign (owner_fk, name, tags, styles, isactive) 
+insert into campaign (owner_fk, name, tags, styles, isactive)
 	VALUES ( 100, 'solid', 'solid', '16 17', 1 ) ;
 
-insert into campaign (owner_fk, name, tags, styles, isactive) 
+insert into campaign (owner_fk, name, tags, styles, isactive)
 	VALUES ( 100, 'floral', 'flowers', '16 17', 0 ) ;
 
-insert into campaign (owner_fk, name, tags, styles, isactive) 
+insert into campaign (owner_fk, name, tags, styles, isactive)
 	VALUES ( 100, 'hearts', 'valentine', '16', 1 ) ;
+
+insert into campaign (owner_fk, name, tags, art_prefix, isactive)
+	VALUES ( 100, 'mommynme', 'mm', 'umm', 1 ) ;
 
